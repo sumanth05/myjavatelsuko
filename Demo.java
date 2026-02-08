d2e9b5a92b3c499f7eeca671dc1ac378bb6b340c
sumanthmadhira5 <sumanthmadhira5@gmail.com>
2026-02-08 17:00:45 +0530

    COnstructurs and strings Methods

--- Demo.java
+++ Demo.java
@@ -326,6 +326,7 @@
 //     }
 // }
 
+import java.lang.classfile.instruction.StackInstruction;
 
 /// method overloading
 
@@ -357,34 +358,497 @@
 //      }
 // }
 
-class caluculator
-{
+// class caluculator
+// {
 
-    int num=5;
+//     int num=5;
 
 
-    public int add(int n1, int n2)
-    {
-        //System.out.println(num);
-        return n1 + n2;
-    }
+//     public int add(int n1, int n2)
+//     {
+//         //System.out.println(num);
+//         return n1 + n2;
+//     }
    
 
-}
+// }
 
-public class Demo
-{
-     public static void main(String a[])
-     {
-        caluculator obj = new caluculator();
-        caluculator  obj1 = new caluculator();
-        int r1 = obj.add(3,4);
-        //System.out.println(r1);
+// public class Demo
+// {
+//      public static void main(String a[])
+//      {
+//         caluculator obj = new caluculator();
+//         caluculator  obj1 = new caluculator();
+//         int r1 = obj.add(3,4);
+//         //System.out.println(r1);
 
-        obj.num = 8;
+//         obj.num = 8;
 
-        System.out.println(obj.num);
-        System.out.println(obj1.num);
+//         System.out.println(obj.num);
+//         System.out.println(obj1.num);
 
-     }
-}
+//      }
+// }
+
+// Array
+
+// public class Demo
+// {
+//     public static void main(String a[])
+//     {
+//         int nums[] = {3,7,2,4};
+//         nums[1] = 6;
+
+//         System.out.println(nums[1]);
+//     }
+// }
+
+// public class Demo
+// {
+//     public static void main(String a[])
+//     {
+//         int nums[] = new int[4];
+//         nums[0] = 4;
+//         nums[1] = 8;
+//         nums[2] = 3;
+//         nums[3] = 9;
+
+
+//         for(int i=0;i<=3;i++)
+//         {
+//             System.out.println(nums[i]);
+
+//         }
+        
+//     }
+// }
+
+
+
+// Multi dimentional Array
+
+
+// public class Demo
+// {
+//     public static void main(String a[])
+//     {
+//         int nums[][] = new int [3][4];
+
+//         for(int i=0;i<3;i++)
+//         {
+//             for(int j=0;j<4;j++)
+//             {
+//                 nums[i][j] = (int)(Math.random() * 10);
+//                 System.out.print(nums[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+
+//         for(int n[] : nums)
+//         {
+//             for(int m: n)
+//             {
+//                 System.out.print(m + " ");
+//             }
+//             System.out.println();
+//         }
+
+        
+//     }
+// }
+
+
+// jagged array
+
+// public class Demo
+// {
+//     public static void main(String a[])
+//     {
+//         int nums[][] = new int [3][]; //jagged array
+
+//         nums[0] = new int[3];
+//         nums[1] = new int[4];
+//         nums[2] = new int[2];
+
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             for(int j=0;j<nums[i].length;j++)
+//             {
+//                 nums[i][j] = (int)(Math.random() * 10);
+//             }
+    
+//         }
+
+//         for(int n[] : nums)
+//         {
+//             for(int m: n)
+//             {
+//                 System.out.print(m + " ");
+//             }
+//             System.out.println();
+//         }
+
+        
+//     }
+// }
+
+
+// 3 Dimentional array
+
+// public class Demo
+// {
+//     public static void main(String a[])
+//     {
+//         int nums[][][] = new int [3][4][5]; // 3D Array
+
+
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             for(int j=0;j<nums[i].length;j++)
+//             {
+//                 nums[i][j] = (int)(Math.random() * 10);
+//             }
+    
+//         }
+
+//         for(int n[] : nums)
+//         {
+//             for(int m: n)
+//             {
+//                 System.out.print(m + " ");
+//             }
+//             System.out.println();
+//         }
+
+        
+//     }
+// }
+
+
+
+// class Student
+// {
+//     int rollno;
+//     String name;
+//     int marks;
+// }
+
+
+// public class Demo
+// {
+//     public static void main(String a[])
+//     {
+//         Student s1 = new Student();
+//         s1.rollno = 1;
+//         s1.name = "Sumanth";
+//         s1.marks = 88;
+
+//         Student s2 = new Student();
+//         s2.rollno = 2;
+//         s2.name = "Madhira";
+//         s2.marks = 67;
+
+//         Student s3 = new Student();
+//         s3.rollno = 3;
+//         s3.name = "Reddy";
+//         s3.marks = 99;
+
+//         Student students[] = new Student[3];
+//         students[0] = s1;
+//         students[1] = s2;
+//         students[2] = s3;
+
+//         for(int i=0;i<students.length;i++)
+//         {
+//             System.out.println(students[i].name + " : " + students[i].marks);
+//         }
+        
+
+
+        // int nums[] = new int[4];
+        // nums[0] = 4;
+        // nums[1] = 8;
+        // nums[2] = 3;
+        // nums[3] = 9;
+
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     System.out.println(nums[i]);
+        // }
+//     }
+// }
+
+
+// For each loop
+
+
+// class Student
+// {
+//     int rollno;
+//     String name;
+//     int marks;
+// }
+
+// public class Demo
+// {
+//     public static void main(String a[])
+//     {
+//         // int nums[] = new int[4];
+//         // nums[0] = 4;
+//         // nums[1] = 8;
+//         // nums[2] = 3;
+//         // nums[3] = 9;
+
+//         // // for(int i=0;i<nums.length;i++)
+//         // // {
+//         // //     System.out.println(nums[i]);
+//         // // }
+
+//         // for(int n : nums)
+//         // {
+//         //     System.out.println(n);
+//         // }
+
+//         Student s1 = new Student();
+//         s1.rollno = 1;
+//         s1.name = "Sumanth";
+//         s1.marks = 88;
+
+//         Student s2 = new Student();
+//         s2.rollno = 2;
+//         s2.name = "Madhira";
+//         s2.marks = 67;
+
+//         Student s3 = new Student();
+//         s3.rollno = 3;
+//         s3.name = "Reddy";
+//         s3.marks = 99;
+
+//         Student students[] = new Student[3];
+//         students[0] = s1;
+//         students[1] = s2;
+//         students[2] = s3;
+
+//         // for(int i=0;i<students.length;i++)
+//         // {
+//         //     System.out.println(students[i].name + " : " + students[i].marks);
+//         // }
+
+//         for(Student stud : students)
+//         {
+//             System.out.println(stud.name + " : " + stud.marks);
+//         }
+//     }
+// }
+
+
+//STRINGS
+
+
+// public class Demo
+// {
+//     public static void main(String a[])
+//     {
+//         String  name = "Sumanth";
+//         name = name + " Reddy";
+//         System.out.println("hello " + name);
+        
+
+
+//     }
+// }
+
+// public class Demo
+// {
+//     public static void main(String a[])
+//     {
+//         StringBuffer sb = new StringBuffer("Sumanth");
+//         sb.append(" Reddy");
+
+        
+
+//          //sb.insert(7, " java ");
+//         //sb.setLength(30);
+
+//         String str = sb.toString();
+
+//         System.out.println(sb);
+      
+//     }
+//}
+
+
+// STATIC KEYWORD
+
+// class Mobile
+// {
+//     String brand;
+//     int price;
+//     static String name;
+
+//     public void show()
+
+//     {
+
+//     System.out.println(brand + " : " + price + " : " + name);
+// }
+// }
+
+// public class Demo
+// {
+//     public static void main(String a[])
+//     {
+//       Mobile obj1 = new Mobile();
+//       obj1.brand = "Apple";
+//       obj1.price = 15000;
+//       Mobile.name = "SmartPhone";
+
+//       Mobile obj2 = new Mobile();
+//       obj2.brand = "Samsung";
+//       obj2.price = 1700;
+//       Mobile.name = "SmartPhone";
+
+//       obj1.name = "Phone";
+
+//       obj1.show();
+//       obj2.show(); 
+
+
+//     }
+// }
+
+
+// STATIC 
+
+// class Mobile
+// {
+//     String brand;
+//     int price;
+//     static String name;
+
+//     static 
+//     {
+//         name ="Phone";
+//         System.out.println("in static block");
+
+//     }
+
+//     public Mobile()
+//     {
+//         brand = "";
+//         price = 200;
+//         System.out.println("in Constructor");
+        
+//     }
+
+//     public void show()
+
+//     {
+
+//     System.out.println(brand + " : " + price + " : " + name);
+//     }
+
+// }
+
+// public class Demo
+// {
+//     public static void main(String a[]) throws ClassNotFoundException
+//     {
+//         Class.forName("Mobile");
+//     //   Mobile obj1 = new Mobile();
+//     //   obj1.brand = "Apple";
+//     //   obj1.price = 15000;
+//     //   Mobile.name = "SmartPhone";
+
+//     //   Mobile obj2 = new Mobile();
+ 
+//     }
+// }
+
+//Static Method
+
+// class Mobile
+// {
+//     String brand;
+//     int price;
+//     static String name;
+
+//     public void show()
+
+//     {
+
+//     System.out.println(brand + " : " + price + " : " + name);
+//     }
+//     public static void show1(Mobile obj)
+//     {
+//         System.out.println(obj.brand + " : " + obj.price + " : " + name);
+//     }
+// }
+
+// public class Demo
+// {
+//     public static void main(String a[])
+//     {
+//       Mobile obj1 = new Mobile();
+//       obj1.brand = "Apple";
+//       obj1.price = 15000;
+//       Mobile.name = "SmartPhone";
+
+//       Mobile obj2 = new Mobile();
+//       obj2.brand = "Samsung";
+//       obj2.price = 1700;
+//       Mobile.name = "SmartPhone";
+
+//       obj1.name = "Phone";
+
+//       obj1.show();
+//       obj2.show(); 
+
+//       Mobile.show1(obj1);
+
+//     }
+// }
+
+
+//ENCASPULATION////
+
+// class Human
+// {
+//     private int age;
+//     private String name;
+
+//     public int getAge() 
+//     {
+//         return age;
+//     }
+//     public void setAge(int a) 
+//     {
+//         Human obj1 = new Human();
+//         obj1.age = age;
+//     }
+//     public String getName() 
+//     {
+//         return name;
+//     }
+//     public void setName(String n) 
+//     {
+//         n = name;
+//     }
+
+    
+
+
+// }
+
+// public class Demo
+// {
+//     public static void main(String a[])
+//     {
+//         Human obj = new Human();
+//         obj.setAge(30);
+//         obj.setName("Sumanth");
+
+//         System.out.println(obj.getName() + " : " + obj.getAge());
+
+//     }
+// }
